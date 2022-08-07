96b69f4d133dffdd018153cb389be55a11a23b00
kkpagaev <kapagaev@gmail.com>
2022-08-07 15:38:48 +0300

    error handle

---
+++ src/filter/bad-request.filter.ts
@@ -0,0 +1,10 @@
+import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+
+@Catch()
+export class BadRequestFilter implements ExceptionFilter {
+  catch(exception: Error, host: ArgumentsHost) {
+    const response = host.switchToHttp().getResponse();
+    response.status(400).json({ message: exception.message });
+
+  }
+}
